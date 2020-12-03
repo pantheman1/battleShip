5c14cf9d86baa8e343c507383e6c750b99ffd744
Peter Anderson <peterburtonanderson@gmail.com>
2020-12-03 11:49:48 -0700

    getMove method near completion

--- starter/humanPlayer.js
+++ starter/humanPlayer.js
@@ -1,14 +1,22 @@
+const readline = require("readline");
+
 class HumanPlayer {
   constructor() {
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
+    this.rl = readline.createInterface(process.stdin, process.stdout);
   }
 
-  getMove() {
+  getMove(processMove /*returns a string*/) {
     // TODO: Ask the user for their move and process the answer using the rl
-    // interface.Invoke the callback function (processMove), passing in
+    // interface. Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
+    // we'll need to do string.split on answer
+    this.rl.question("What is your move?", (answer) => {
+      processMove(answer.split(","));
+      this.rl.close() //is this necessary? They do not have it in their answer code.
+    })
   }
 
   processGameOver(isWon) {
